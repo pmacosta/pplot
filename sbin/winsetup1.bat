35d1290472ea22fdd50bc23eccf6227d712f29ab
Pablo Acosta-Serafini <pmasdev@gmail.com>
2017-02-16 16:14:53 -0500

    Windows continuous integration fixes

--- sbin/winsetup1.bat
+++ sbin/winsetup1.bat
@@ -1,24 +1,42 @@
+REM [START IGNORE]
 set PYTHON_MAJOR=2
 set INTERP=py27
 set PYVER=2.7
-
+REM [STOP IGNORE]
+REM install:
+REM ###
+REM # Set up environment variables
+REM ###
+set
 set PYTHONCMD=python
 set PIPCMD=pip
-set REPO_DIR=%CD%
-set RESULTS_DIR=%REPO_DIR%\results
-set PKG_NAME=pplot
-set PYTHON_SITE_PACKAGES=C:\Anaconda\envs\%INTERP%\lib\site-packages
-set BIN_DIR=C:\Anaconda\envs\%INTERP%\Scripts
-set SOURCE_DIR=%PYTHON_SITE_PACKAGES%\%PKG_NAME%
-set VIRTUALENV_DIR=\Anaconda\envs\%INTERP%
-set EXTRA_DIR=%VIRTUALENV_DIR%\usr\share\%PKG_NAME%
-set PYTHONPATH=%PYTHONPATH%;%PYTHON_SITE_PACKAGES%;%EXTRA_DIR%;%EXTRA_DIR%\tests;%EXTRA_DIR%\docs;%EXTRA_DIR%\docs\support
 set PYTESTCMD=py.test
-set COV_FILE=%SOURCE_DIR%\.coveragerc_ci_%INTERP%
+set REPO_DIR=%CD%
+for %%i in (%REPO_DIR%) do @echo %%~ni> pkg_name.txt
+set /p PKG_NAME=<pkg_name.txt
+set RESULTS_DIR=%REPO_DIR%\results
 set MAIN_REQUIREMENTS_FILE=%REPO_DIR%\requirements\main_%INTERP%.pip
 set TESTS_REQUIREMENTS_FILE=%REPO_DIR%\requirements\tests_%INTERP%.pip
-
+set DOCS_REQUIREMENTS_FILE=%REPO_DIR%\requirements\docs_%INTERP%.pip
+set CITMP=%REPO_DIR%\CITMP
+if not exist "%CITMP%" mkdir %CITMP%
+echo "INTERP=%INTERP%"
+echo "PKG_NAME=%PKG_NAME%"
+echo "PYTHONCMD=%PYTHONCMD%"
+echo "PIPCMD=%PIPCMD%"
+echo "PYTESTCMD=%PYTESTCMD%"
+echo "REPO_DIR=%REPO_DIR%"
+echo "RESULTS_DIR=%RESULTS_DIR%"
+echo "MAIN_REQUIREMENTS_FILE=%MAIN_REQUIREMENTS_FILE%"
+echo "TESTS_REQUIREMENTS_FILE=%TESTS_REQUIREMENTS_FILE%"
+echo "DOCS_REQUIREMENTS_FILE=%DOCS_REQUIREMENTS_FILE%"
+echo "CITMP=%CITMP%"
+REM ###
+REM # Install tools and dependencies of package dependencies
+REM ###
+set PATH=C:\\Miniconda-x64;C:\\Miniconda-x64\\Scripts;%PATH%
 conda update -y conda
 conda env remove -y --name %INTERP%
 conda create -y --name %INTERP% python=%PYVER% numpy scipy matplotlib
 activate %INTERP%
+which python
